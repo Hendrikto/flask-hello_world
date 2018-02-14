0e6943d725575a88841a1743ea785d3b53a06220
Hendrikto <hendrik.to@gmail.com>
2018-02-14 17:29:57 +0100

    Add a rudimentary visit counter

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+visits = 0
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/counter")
+def counter():
+    global visits
+    visits += 1
+    return f"<b>Visits:</b> {visits}"
